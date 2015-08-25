b355ae417d995acc93acf7d6f13f6496f9aab472
xhardcorex <nik@MacBook-Air-Nik.local>
2015-08-25 15:12:57 +0300

    Initial Commit

---
+++ Lesoon35 UITableViewSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesoon35 UITableViewSearch
+//
+//  Created by Nik on 25.08.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesoon35 UITableViewSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesoon35 UITableViewSearch
+//
+//  Created by Nik on 25.08.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
